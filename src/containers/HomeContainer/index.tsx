b9c86c838484d654b0b85355dd46f46f8d56274a
hellomac87 <hellomac87@gmail.com>
2022-02-17 17:32:38 +0900

    feat: route to quiz start page

--- src/containers/HomeContainer/index.tsx
+++ src/containers/HomeContainer/index.tsx
@@ -1,7 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
+import { routes } from '../../constants/routes';
+
 import Home from '../../components/home';
 
 function HomeContainer() {
-    return <Home />;
+    const navigate = useNavigate();
+
+    const handleClickStart = () => {
+        navigate(routes.quiz);
+        console.log('handleClickStart');
+    };
+
+    return <Home onClick={handleClickStart} />;
 }
 
 export default HomeContainer;
