2995532cbb1d5581b2b6fe9fb24b1cebce7309b6
hellomac87 <hellomac87@gmail.com>
2022-02-19 22:59:40 +0900

    feat: retry logic

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,3 +1,4 @@
+import { QuizType } from 'src/types/quiz';
 import create from 'zustand';
 
 export type Answer = {
@@ -6,23 +7,45 @@ export type Answer = {
 };
 
 export type GlobalState = {
+    quizzes: QuizType[];
+    setQuizzes(quizzes: QuizType[]): void;
     myAnswersHistory: Array<Answer>;
     setMyAnswersHistory(answer: Answer): void;
+    resetMyAnswerHistory(): void;
     seconds: number;
     setSeconds(time: number): void;
+    isRetry: boolean;
+    setIsRetry(flag: boolean): void;
 };
 
 export const useStore = create<GlobalState>((set) => ({
+    quizzes: [],
+    setQuizzes: (quizzes: QuizType[]) =>
+        set((state) => ({
+            ...state,
+            quizzes,
+        })),
     myAnswersHistory: [],
     setMyAnswersHistory: (answer: Answer) =>
         set((state) => ({
             ...state,
             myAnswersHistory: [...state.myAnswersHistory, answer],
         })),
+    resetMyAnswerHistory: () =>
+        set((state) => ({
+            ...state,
+            myAnswersHistory: [],
+        })),
     seconds: 0,
     setSeconds: (time: number) =>
         set((state) => ({
             ...state,
             seconds: time,
+        })),
+    isRetry: false,
+    setIsRetry: (flag: boolean) =>
+        set((state) => ({
+            ...state,
+            isRetry: flag,
         })),
 }));
