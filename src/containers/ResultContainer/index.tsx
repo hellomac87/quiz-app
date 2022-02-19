d6f1baf1f0a50c6aceaee3a52f9fd34d0a2a4690
hellomac87 <hellomac87@gmail.com>
2022-02-19 20:09:36 +0900

    feat: ResultCorrctAmount

--- src/containers/ResultContainer/index.tsx
+++ src/containers/ResultContainer/index.tsx
@@ -1,15 +1,20 @@
+import ResultCorrctAmount from 'src/components/result/ResultCorrectAmount';
 import ResultTime from 'src/components/result/ResultTime';
 import { getHHMMSSFromSeconds } from 'src/libs/result';
 import { useStore } from 'src/store';
 
 function ResultContainer() {
-    const { seconds } = useStore((state) => state);
+    const { seconds, myAnswersHistory } = useStore((state) => state);
+
+    const correctAmount = myAnswersHistory.filter((answer) => answer.correct).length;
+    const incorrectAmount = myAnswersHistory.filter((answer) => !answer.correct).length;
 
     const time = getHHMMSSFromSeconds(seconds);
 
     return (
         <>
             <ResultTime time={time} />
+            <ResultCorrctAmount correctAmount={correctAmount} incorrectAmount={incorrectAmount} />
         </>
     );
 }
