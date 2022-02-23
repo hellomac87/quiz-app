3ac1bd0c10d9a67408d2103f40501c114fbf02e3
hellomac87 <hellomac87@gmail.com>
2022-02-23 23:18:46 +0900

    style: result style

--- src/components/result/ResultAction/index.tsx
+++ src/components/result/ResultAction/index.tsx
@@ -1,5 +1,7 @@
 import Button from 'src/components/common/Button';
 
+import styles from './ResultAction.module.css';
+
 type Props = {
     onClickRetry(): void;
     onClickOtherQuiz(): void;
@@ -7,7 +9,7 @@ type Props = {
 
 function ResultAction({ onClickRetry, onClickOtherQuiz }: Props) {
     return (
-        <div className='flex justify-center gap-2 mb-4'>
+        <div className={styles.container}>
             <Button onClick={onClickRetry}>{'같은 문제 다시 풀기'}</Button>
             <Button onClick={onClickOtherQuiz} variant='outline'>
                 {'다른 문제 풀기'}
