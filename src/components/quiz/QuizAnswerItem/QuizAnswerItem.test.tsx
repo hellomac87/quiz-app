1b825a1760eb680793ab258cde696b0e919a4041
hellomac87 <hellomac87@gmail.com>
2022-02-24 00:07:35 +0900

    feat:  QuizAnswerItem test

--- src/components/quiz/QuizAnswerItem/QuizAnswerItem.test.tsx
+++ src/components/quiz/QuizAnswerItem/QuizAnswerItem.test.tsx
@@ -10,9 +10,23 @@ const onClick = () => {
 const answer = 'Is it correct to write test code like this?';
 
 describe('QuizAnswerItem test', () => {
-    test('snapshot: no Props', () => {
+    test('snapshot: correct', () => {
         const tree = renderer
             .create(<QuizAnswerItem onClick={onClick} answer={answer} correct={AnswerResult.CORRECT} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('snapshot: incorrect', () => {
+        const tree = renderer
+            .create(<QuizAnswerItem onClick={onClick} answer={answer} correct={AnswerResult.INCORRECT} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('snapshot: none', () => {
+        const tree = renderer
+            .create(<QuizAnswerItem onClick={onClick} answer={answer} correct={AnswerResult.NONE} />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
